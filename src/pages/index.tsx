a218276b780c0850f3abb2ac252b9ec29b3d219f
mardsonferreira <mardsonferreira25@gmail.com>
2021-10-02 12:17:17 -0300

    Creating Header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from "next/head";
 
-import styles from "../syles/home.module.scss";
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Início | ig.news</title>
       </Head>
-      <h1 className={styles.title}>
+      <h1>
         Hello <span>world</span>
       </h1>
     </>
