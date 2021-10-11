b7b87c1b4f1d649ac3e68913053b3b32cc98f98a
mardsonferreira <mardsonferreira25@gmail.com>
2021-10-11 09:18:34 -0300

    Using prismic API

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,28 +1,33 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import Prismic from "@prismicio/client";
+
+import { getPrismicClient } from "../../services/prismic";
+
 import styles from "./styles.module.scss";
 
 export default function Post() {
   return (
     <>
       <Head>
-        <title>Posts | Ignews</title>  
-      </Head> 
+        <title>Posts | Ignews</title>
+      </Head>
 
       <main className={styles.container}>
         <div className={styles.posts}>
           <a href="#">
             <time> 11 de outubro</time>
-            <strong>Creating  a new post</strong>
+            <strong>Creating a new post</strong>
             <p>A simple paragraph</p>
           </a>
           <a href="#">
             <time> 11 de outubro</time>
-            <strong>Creating  a new post</strong>
+            <strong>Creating a new post</strong>
             <p>A simple paragraph</p>
           </a>
           <a href="#">
             <time> 11 de outubro</time>
-            <strong>Creating  a new post</strong>
+            <strong>Creating a new post</strong>
             <p>A simple paragraph</p>
           </a>
         </div>
@@ -30,3 +35,21 @@ export default function Post() {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at("document.type", "p")],
+    {
+      fetch: ["title", "content"],
+      pageSize: 100,
+    }
+  );
+
+  console.log(JSON.stringify(response, null, 2));
+
+  return {
+    props: {},
+  };
+};
